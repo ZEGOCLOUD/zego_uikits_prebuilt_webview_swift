b34be3e9dba89a27c56fa49d90d5131174742990
zhaowei <choui20016514@gmail.com>
2022-12-21 11:38:39 +0800

    create project

--- myWebviewWebRTC/ViewController.swift
+++ myWebviewWebRTC/ViewController.swift
@@ -1,19 +1,26 @@
-//
-//  ViewController.swift
-//  myWebviewWebRTC
-//
-//  Created by 赵伟 on 2022/12/21.
-//
+   //  ViewController.swift
+   //  myWebviewWebRTCIOS
+   //
+   //  Created by 赵伟 on 2022/12/19.
+   //
 
-import UIKit
+   import UIKit
+   import WebKit
 
-class ViewController: UIViewController {
+   class ViewController: UIViewController, WKUIDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+       var wkwebview: WKWebView!
 
-
-}
-
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           let request = URLRequest(url: URL(string: "https://zegocloud.github.io/zego_uikit_prebuilt_web/video_conference/index.html?roomID=zegocloud&role=Host")!)
+           wkwebview.load(request)
+       }
+       override func loadView() {
+               let webConfiguration = WKWebViewConfiguration()
+               webConfiguration.allowsInlineMediaPlayback = true //** Added as an example for your case
+               wkwebview = WKWebView(frame: .zero, configuration: webConfiguration)
+               wkwebview.uiDelegate = self
+               view = wkwebview
+           }
+   }
